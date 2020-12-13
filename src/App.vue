0f8040a2a1b085302698d61ee331d1ef00c95f07
yutoogi <jacoyutorius@gmail.com>
2020-12-13 15:36:11 +0900

    edit top page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/amplify.png">
+    <HelloWorld msg="AWS Amplify authentication sandbox"/>
   </div>
 </template>
 
