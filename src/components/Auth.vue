aa1c121e4fccff32ec12cbb3ee5a890cedf973ab
yutoogi <jacoyutorius@gmail.com>
2020-12-14 01:32:22 +0900

    SignUpフォームの表示内容変更

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,7 +1,24 @@
 <template>
   <amplify-authenticator>
-    <div>
-      <amplify-sign-out></amplify-sign-out>
-    </div>
+    <amplify-sign-out></amplify-sign-out>
+    <amplify-sign-up
+      slot="sign-up"
+      username-alias="email"
+      :form-fields.prop="formFields"
+    ></amplify-sign-up>
   </amplify-authenticator>
 </template>
+
+<script>
+export default {
+  name: 'Auth',
+  data() {
+    return {
+      formFields: [
+        { type: "email" },
+        { type: "password" },
+      ]
+    }
+  }
+}
+</script>
